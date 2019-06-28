2467bc0bc1a774db72efddf9dfa4eb516f99c6d9
Joe Roberts <j.m.roberts2@keele.ac.uk>
2019-06-28 09:25:57 +0100

    Refining results_table function and add required packages to description.

--- R/results_table.R
+++ R/results_table.R
@@ -28,73 +28,73 @@
 #' @export
 #'
 results_table = function(x) {
-  data = read_delim(
+  data = readr::read_delim(
     file.choose(),
     delim = " ",
     col_names = c("A", "B", "C", "D", "E"),
-    col_types = cols("D" = col_integer())
+    col_types = readr::cols("D" = readr::col_integer())
   )
 
   data = data %>%
-    complete(nesting(A, B, C), D = seq(min(D), max(D), 1L)) %>%
-    arrange(is.na(E)) %>%
-    mutate(E = replace_na(E, 0))
+    tidyr::complete(tidyr::nesting(A, B, C), D = seq(min(D), max(D), 1L)) %>%
+    dplyr::arrange(is.na(E)) %>%
+    dplyr::mutate(E = tidyr::replace_na(E, 0))
 
   times_entered = data %>%
-    complete(nesting(A, B, C), D = seq(min(D), max(D), 1L)) %>%
-    arrange(is.na(E)) %>%
-    mutate(E = replace_na(E, 0)) %>%
-    add_count(D) %>%
-    mutate(n = (E != 0) * n) %>%
-    group_by(D) %>%
-    summarise_at(vars(n), .funs = mean) %>%
-    pull(n)
+    tidyr::complete(tidyr::nesting(A, B, C), D = seq(min(D), max(D), 1L)) %>%
+    dplyr::arrange(is.na(E)) %>%
+    dplyr::mutate(E = tidyr::replace_na(E, 0)) %>%
+    dplyr::add_count(D) %>%
+    dplyr::mutate(n = (E != 0) * n) %>%
+    dplyr::group_by(D) %>%
+    dplyr::summarise_at(dplyr::vars(n), .funs = mean) %>%
+    dplyr::pull(n)
 
-  times_entered = tibble(times_entered)
+  times_entered = tibble::tibble(times_entered)
 
   zones = data %>%
-    mutate(control = D != C)
+    dplyr::mutate(control = D != C)
 
-  by_zone = group_by(zones, D)
+  by_zone = dplyr::group_by(zones, D)
 
-  sum_zone_times = summarise(by_zone, time_secs = sum(E)) %>%
-    mutate(time_mins = time_secs / 60)
+  sum_zone_times = dplyr::summarise(by_zone, time_secs = sum(E)) %>%
+    dplyr::mutate(time_mins = time_secs / 60)
 
   treatment_zone = zones %>%
-    filter(control == F) %>%
-    mutate("Olfactometer Zone" = D) %>%
-    mutate(Treatment = sum(E)) %>%
-    mutate("Treatment Arm" = "T")
+    dplyr::filter(control == F) %>%
+    dplyr::mutate("Olfactometer Zone" = D) %>%
+    dplyr::mutate(Treatment = sum(E)) %>%
+    dplyr::mutate("Treatment Arm" = "T")
 
   tbl_one = treatment_zone %>%
-    select("Olfactometer Zone", "Treatment Arm")
+    dplyr::select("Olfactometer Zone", "Treatment Arm")
 
   control_zones = zones %>%
-    filter(control == T) %>%
-    mutate("Olfactometer Zone" = D) %>%
-    mutate(Treatment = sum(E)) %>%
-    mutate("Treatment Arm" = " ")
+    dplyr::filter(control == T) %>%
+    dplyr::mutate("Olfactometer Zone" = D) %>%
+    dplyr::mutate(Treatment = sum(E)) %>%
+    dplyr::mutate("Treatment Arm" = " ")
 
   tbl_two = control_zones %>%
-    select("Olfactometer Zone", "Treatment Arm")
+    dplyr::select("Olfactometer Zone", "Treatment Arm")
 
-  results_tbl = bind_rows(tbl_one, tbl_two) %>%
-    distinct
+  results_tbl = dplyr::bind_rows(tbl_one, tbl_two) %>%
+    dplyr::distinct
 
-  ordered_zones = arrange(results_tbl, results_tbl$`Olfactometer Zone`)
+  ordered_zones = dplyr::arrange(results_tbl, results_tbl$`Olfactometer Zone`)
 
-  results_table = bind_cols(sum_zone_times, times_entered, ordered_zones)
+  results_table = dplyr::bind_cols(sum_zone_times, times_entered, ordered_zones)
 
   results_table = results_table %>%
-    select(D, time_secs, time_mins, times_entered, `Treatment Arm`) %>%
-    rename(!!"Olfactometer Zone" := D) %>%
-    rename(!!"Total Time in Zone (secs)" := time_secs) %>%
-    rename(!!"Total Time in Zone (mins)" := time_mins) %>%
-    rename(!!"No. of Times Zone Entered" := times_entered)
+    dplyr::select(D, time_secs, time_mins, times_entered, `Treatment Arm`) %>%
+    dplyr::rename(!!"Olfactometer Zone" := D) %>%
+    dplyr::rename(!!"Total Time in Zone (secs)" := time_secs) %>%
+    dplyr::rename(!!"Total Time in Zone (mins)" := time_mins) %>%
+    dplyr::rename(!!"No. of Times Zone Entered" := times_entered)
 
-  final_table = kable(head(results_table), format = "markdown", digits = 2, align = "c")
+  final_table = knitr::kable(utils::head(results_table), format = "markdown", digits = 2, align = "c")
 
-  print(final_table)
+  base::print(final_table)
 
-  export(results_table, "Summary Results Table.xlsx")
+  rio::export(results_table, "Summary Results Table.xlsx")
 }
