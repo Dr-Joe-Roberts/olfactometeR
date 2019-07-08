d4b95b61f98f967e6974feb5882b072dfc6858cc
Joe Roberts <j.m.roberts2@keele.ac.uk>
2019-07-08 17:28:52 +0100

    Updated record_four_arm function.

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,4 @@
 .onLoad <- function(libname = find.package("olfactometeR"), pkgname = "olfactometeR") {
   utils::globalVariables(c("A", "B", "C", "D", "E", "n", "time_secs", "control", "time_mins", "Treatment Arm"))
 }
+
